419f307d039523bbb42ca7f10a6ae8fe4692ab38
dilhan.jayathilake <dilhan.jayathilake@carsales.com.au>
2018-04-18 16:34:07 +1000

    first commit

---
+++ SadahamWeb/Infrastructure/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SadahamWeb.Infrastructure
+{
+    public class Constants
+    {
+        public const string AppName = "Sample";
+    }
+}
