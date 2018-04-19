1e4147670e3436b5123915ecfe22e34577703f2d
Developer <developer@crystald.com.au>
2018-04-20 00:56:30 +1000

    deploy sh file

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash  
+echo "Build docker image"  
+
+docker build https://github.com/dilhan2013/sadaham.git#master -t dilhan2013/sadahamweb:latest
+
+echo "Docker stack deploy"
+
+docker stack deploy -c docker-compose.yml web
+
+echo "Expose port 80"
+
+docker service update --publish-add published=80,target=80 web_webapp
